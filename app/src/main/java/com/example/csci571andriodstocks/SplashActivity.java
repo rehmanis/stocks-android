d1d70f4944e51a05c2a254a3a040b15ac4508452
Shamsuddin Rehmani <rehmani@usc.edu>
2020-11-10 20:35:25 -0500

    added the logo and splash screen for stocks app

---
+++ app/src/main/java/com/example/csci571andriodstocks/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.csci571andriodstocks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
