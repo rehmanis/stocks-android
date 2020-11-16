9778578fc6cbba2ef36240c4b2dc0b59b61bd897
Shamsuddin Rehmani <rehmani@usc.edu>
2020-11-15 21:38:02 -0500

    autocomplete now working with simple hardcoded list

--- app/src/main/java/com/example/csci571andriodstocks/MainActivity.java
+++ app/src/main/java/com/example/csci571andriodstocks/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.csci571andriodstocks;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,6 +12,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.SearchView;
 
 import java.util.List;
@@ -20,6 +24,16 @@ import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapt
 public class MainActivity extends AppCompatActivity implements WatchlistSection.ClickListener {
 
     private SectionedRecyclerViewAdapter sectionedAdapter;
+    String restaurants[] = {
+            "KFC",
+            "Dominos",
+            "Pizza Hut",
+            "Burger King",
+            "Subway",
+            "Dunkin' Donuts",
+            "Starbucks",
+            "Cafe Coffee Day"
+    };
 
 
     @Override
@@ -46,12 +60,22 @@ public class MainActivity extends AppCompatActivity implements WatchlistSection.
         inflater.inflate(R.menu.options_menu, menu);
 
         // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        ArrayAdapterSearchView searchView = (ArrayAdapterSearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, restaurants);
+        searchView.setAdapter(adapter);
+
+        searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                searchView.setText(adapter.getItem(position).toString());
+
+            }
+        });
+
         return true;
     }
 
